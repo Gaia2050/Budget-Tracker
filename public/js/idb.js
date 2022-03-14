6b7355bc7c3a3d86a5e0d43e6a2a7856b56eee4f
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-03-13 22:02:24 -0400

    bcs

---
+++ public/js/idb.js
@@ -0,0 +1,27 @@
+let db;
+
+const request = indexDB.open('budget-tracker', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', {autoIncrement: true});
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.online) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord (record) {
+    const transaction = db.transaction(['new_transaction', 'readwrite']);
+    const transactionObjectStore = transaction.ObjectStore('new_transaction');
+    transactionObjectStore.add(record);
+};
+
+window.addEventListener('online', uploadTransaction);
